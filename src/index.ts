bf079db97064cbb19670e0328b215b0508741643
qinran0423 <1156107187@qq.com>
2021-09-12 22:27:49 +0800

    cookies set

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,47 @@
+let defaultAttributes
 
-export function cookieget(name) {
+function set(name, val, attributes) {
+  if (typeof document === 'undefined') {
+    return
+  }
+
+  attributes = Object.assign(attributes, defaultAttributes)
+
+  name = encodeURIComponent(name)
+    .replace(/%(2[346B]|5E|60|7C)/g, decodeURIComponent)
+    .replace(/[()]/g, escape)
+
+  let stringifiedAttributes = ''
+  for (const attributeName in attributes) {
+    stringifiedAttributes += '; ' + attributeName
+
+    stringifiedAttributes += '=' + attributes[attributeName].split(';')[0]
+  }
+
+
+  document.cookie = name + '=' + val + stringifiedAttributes
+}
+
+
+function get(name) {
   const cookies = document.cookie ? document.cookie.split('; ') : []
   const jar = {}
   for (let index = 0; index < cookies.length; index++) {
     const parts = cookies[index].split('=');
     const value = parts.slice(1).join('=')
-    jar[parts[0]] = value
+    const found = decodeURIComponent(parts[0])
+    jar[found] = value
   }
   return jar[name]
 }
+
+function init(converter, defaultAttributes) {
+  defaultAttributes = defaultAttributes
+  return {
+    set,
+    get
+  }
+}
+
+
+export default init({}, { path: '/' })
--- src/test/index.spec.ts
+++ src/test/index.spec.ts
@@ -1,9 +1,22 @@
 
-import { cookieget } from '../index'
+import Cookies from '../index'
 describe('cookie', () => {
   it('happy path', () => {
     document.cookie = "name=mick"
-    expect(cookieget('name')).toBe('mick')
+    expect(Cookies.get('name')).toBe('mick')
+  })
+
+
+  it('set', () => {
+    Cookies.set('name', 'randy')
+    expect(Cookies.get('name')).toBe('randy')
+    Cookies.set('age', 11)
+    expect(Cookies.get('age')).toBe('11')
+    Cookies.set('%2%3', 1)
+    expect(Cookies.get('%2%3')).toBe('1')
   })
 })
+
+
+
 
